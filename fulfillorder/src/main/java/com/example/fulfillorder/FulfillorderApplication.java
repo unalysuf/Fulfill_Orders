2e444789125633d9b11b35f6e9cd67d0f2eeea38
unalysuf <unalysuf@gmail.com>
2023-05-16 14:59:56 +0300

    first commit

---
+++ fulfillorder/src/main/java/com/example/fulfillorder/FulfillorderApplication.java
@@ -0,0 +1,13 @@
+package com.example.fulfillorder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FulfillorderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FulfillorderApplication.class, args);
+	}
+
+}
---
+++ fulfillorder/src/test/java/com/example/fulfillorder/FulfillorderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.fulfillorder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FulfillorderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
