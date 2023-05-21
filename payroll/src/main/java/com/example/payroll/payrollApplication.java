dcac5c75367584fd283a05961d8db42546bf4ff8
unalysuf <unalysuf@gmail.com>
2023-05-21 17:42:40 +0300

    Chaning Repository Name and Project Name Done

--- payroll/src/main/java/com/example/payroll/payrollApplication.java
+++ payroll/src/main/java/com/example/payroll/payrollApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class FulfillorderApplication {
+public class payrollApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(FulfillorderApplication.class, args);
+		SpringApplication.run(payrollApplication.class, args);
 	}
 
 }
